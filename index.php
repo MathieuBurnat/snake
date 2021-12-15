5cd8ac1fde324e343601881d8cade264bd0b73c9
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-12-15 18:08:07 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Snake</title>
+</head>
+    <body>
+        <h2> Hello There. </h2>    
+        <h4> Obiwan Kenobi !?</h4>    
+    </body>
+    
+</html>
