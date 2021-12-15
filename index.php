ade498a54b579017a1251abf94b98fb624a974ff
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-12-15 18:23:51 +0100

    Create css variables

--- index.php
+++ index.php
@@ -7,12 +7,17 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
+<body class="bg-main">
     <h1 class="text-3xl font-bold underline bg-emerald-400">
         Hello world!
     </h1>
+    
     <h2> Hello There. </h2>
     <h4> Obiwan Kenobi !?</h4>
 </body>
+
+<style>
+    @import "styles/main.css";
+</style>
 
 </html>
