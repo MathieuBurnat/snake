ddb41b9670ed73bfc63784d7e3480c9fb2a1f300
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-12-20 15:42:35 +0100

    Import ajax

--- components/header.php
+++ components/header.php
@@ -6,6 +6,18 @@
         <a href="play" class="rounded-md bg-button px-8 py-1">Play</a>
     </div>
     <div class="flex-none w-14">
-        Menu?Info
+        <svg xmlns="http://www.w3.org/2000/svg" id="menu" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd" />
+        </svg>
     </div>
 </div>
+
+<script>
+    $('#menu').click(function(){ launchMenu(); });
+
+    function launchMenu(){
+        console.log("Menu");
+    }
+</script>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Snake</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 
