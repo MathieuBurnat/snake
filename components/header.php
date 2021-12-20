a97cda41e8db3816e61a037a0affcb8b68a9dbd4
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-12-20 14:24:47 +0100

    Add the play button

--- components/header.php
+++ components/header.php
@@ -1,11 +1,11 @@
 <div class="flex bg-content h-16 text-center align-self items-center">
-  <div class="flex-none w-14">
-    Snake
-  </div>
-  <div class="grow">
-    Play Button
-  </div>
-  <div class="flex-none w-14">
-    Menu?Info
-  </div>
+    <div class="flex-none w-14">
+        Snake
+    </div>
+    <div class="grow">
+        <a href="play" class="rounded-md bg-button px-8 py-1">Play</a>
+    </div>
+    <div class="flex-none w-14">
+        Menu?Info
+    </div>
 </div>
