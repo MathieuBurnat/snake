e1bcb783323d85abba6ddbd6b4b9116efe7971dc
Mathieu <Mathieu.Burnat@cpnv.ch>
2022-01-12 10:25:56 +0100

    Add lorems

--- screens/about.php
+++ screens/about.php
@@ -1,5 +1,8 @@
 <div>
     <h1>About Us's section</h1>
+    <div class="text">
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sapiente error similique officiis suscipit cumque ab, veritatis alias, placeat commodi dicta maxime nulla quia! Placeat ipsa ut doloremque saepe numquam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum maiores numquam odio harum maxime deserunt error provident nisi eum omnis, unde inventore at quas quaerat rerum ipsa totam repudiandae nihil.</p>
+    </div>
     <div>
         <div class="px-32 py-20 grid grid-flow-col">
           <div class="max-w-xs rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer">
