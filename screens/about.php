e42febabe8228959a4feb612391b37c6280695d9
Mathieu <Mathieu.Burnat@cpnv.ch>
2022-01-17 17:02:56 +0100

    Create the help section

--- screens/about.php
+++ screens/about.php
@@ -1,7 +1,7 @@
 <div class="">
   <div class="flex flex-wrap w-auto">
     <h1>About Us's section</h1>
-    <div class="text">
+    <div class="text-aright">
       <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sapiente error similique officiis suscipit cumque
         ab, veritatis alias, placeat commodi dicta maxime nulla quia! Placeat ipsa ut doloremque saepe numquam. Lorem
         ipsum dolor sit, amet consectetur adipisicing elit. Illum maiores numquam odio harum maxime deserunt error
