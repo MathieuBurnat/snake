199d2af1762b5d4c4b3ac65e72e165b57ae325a9
Mathieu <Mathieu.Burnat@cpnv.ch>
2022-01-17 18:06:22 +0100

    fill How to play's section

--- screens/learn.php
+++ screens/learn.php
@@ -1,3 +1,19 @@
-<div>
+<div class="flex flex-wrap w-auto">
     <h1>How to play's section</h1>
+
+    <div class="text-aleft">
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sapiente error similique officiis suscipit
+            cumque
+            ab, veritatis alias, placeat commodi dicta maxime nulla quia! Placeat ipsa ut doloremque saepe numquam.
+            Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit. Illum maiores numquam odio harum maxime deserunt error
+            provident nisi eum omnis, unde inventore at quas quaerat rerum ipsa totam repudiandae nihil.</p>
+    </div>
+    <div class="text-aright">
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sapiente error similique officiis suscipit
+            cumque
+            ab, veritatis alias, placeat commodi dicta maxime nulla quia! Placeat ipsa ut doloremque saepe numquam.
+            Lorem
+    </div>
+
 </div>
