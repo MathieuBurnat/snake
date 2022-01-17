2feabb32c20719de466b803d20fbad94a154a3a3
Mathieu <Mathieu.Burnat@cpnv.ch>
2022-01-17 15:49:55 +0100

    Refactor the flex stack

--- screens/about.php
+++ screens/about.php
@@ -1,54 +1,61 @@
 <div class="">
-  <h1>About Us's section</h1>
-  <div class="text">
-    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sapiente error similique officiis suscipit cumque
-      ab, veritatis alias, placeat commodi dicta maxime nulla quia! Placeat ipsa ut doloremque saepe numquam. Lorem
-      ipsum dolor sit, amet consectetur adipisicing elit. Illum maiores numquam odio harum maxime deserunt error
-      provident nisi eum omnis, unde inventore at quas quaerat rerum ipsa totam repudiandae nihil.</p>
+  <div class="flex flex-wrap w-auto">
+    <h1>About Us's section</h1>
+    <div class="text">
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sapiente error similique officiis suscipit cumque
+        ab, veritatis alias, placeat commodi dicta maxime nulla quia! Placeat ipsa ut doloremque saepe numquam. Lorem
+        ipsum dolor sit, amet consectetur adipisicing elit. Illum maiores numquam odio harum maxime deserunt error
+        provident nisi eum omnis, unde inventore at quas quaerat rerum ipsa totam repudiandae nihil.</p>
+    </div>
   </div>
 
   <div class="flex">
-    <div class="flex-wrap w-auto">
+    <div class="flex-auto w-100">
+      <div class="flex-wrap">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="https://www.thesprucepets.com/thmb/_yrib2KGkS4VJSgCQEl59KPUPOU=/1885x1414/smart/filters:no_upscale()/GettyImages-135630198-5ba7d225c9e77c0050cff91b.jpg"
+                alt="Racoon" style="height: 300px;">
+              <div class=" py-4 px-4 bg-white">
+                <h3 class="text-lg font-semibold text-gray-600"> Dimitri </h3>
+                <p class="mt-4 text-lg font-thin">Développeur full stack, joueur de guitare</p>
+              </div>
+            </div>
+            <div class="flip-card-back">
+              <h1>Dimitri H</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui ea officiis natus excepturi quae! Iure
+                dicta dolor sint nemo? Quod neque aperiam id officia. Amet, in! Eveniet debitis dolor nisi.</p>
+              <p>We love that guy</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex-auto w-100">
       <div class="flip-card">
         <div class="flip-card-inner">
           <div class="flip-card-front">
-            <img
-              src="https://www.thesprucepets.com/thmb/_yrib2KGkS4VJSgCQEl59KPUPOU=/1885x1414/smart/filters:no_upscale()/GettyImages-135630198-5ba7d225c9e77c0050cff91b.jpg"
-              alt="Racoon" style="height: 300px;">
-            <div class=" py-4 px-4 bg-white">
-            <h3 class="text-lg font-semibold text-gray-600"> Dimitri </h3>
-            <p class="mt-4 text-lg font-thin">Développeur full stack, joueur de guitare</p>
+            <img src="https://pbs.twimg.com/profile_images/697135903135797248/UcOjql3O_400x400.jpg"
+              alt="Sneaky Snake" style="height: 300px;">
+            <div class="py-4 px-4 bg-white">
+              <h3 class="text-lg font-semibold text-gray-600">Mathieu</h3>
+              <p class="mt-4 text-lg font-thin">Développeur endurcit et passionné de serpent</p>
+            </div>
           </div>
-        </div>
-        <div class="flip-card-back">
-          <h1>Dimitri H</h1>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui ea officiis natus excepturi quae! Iure dicta dolor sint nemo? Quod neque aperiam id officia. Amet, in! Eveniet debitis dolor nisi.</p>
-          <p>We love that guy</p>
+          <div class="flip-card-back">
+            <h1>Mathieu B</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, dolor asperiores voluptate
+              doloremque a dignissimos quos, aut expedita accusantium impedit ducimus tempora! Fuga modi architecto
+              eligendi. Nostrum, debitis cum! A.</p>
+            <p>Architect & Engineer</p>
+          </div>
         </div>
       </div>
     </div>
   </div>
-
-  <div class="flex-1 w-auto">
-    <div class="flip-card">
-      <div class="flip-card-inner">
-        <div class="flip-card-front">
-          <img src="https://pbs.twimg.com/profile_images/697135903135797248/UcOjql3O_400x400.jpg" alt="Sneaky Snake"
-            style="height: 300px;">
-          <div class="py-4 px-4 bg-white">
-            <h3 class="text-lg font-semibold text-gray-600">Mathieu</h3>
-            <p class="mt-4 text-lg font-thin">Développeur endurcit et passionné de serpent</p>
-          </div>
-        </div>
-        <div class="flip-card-back">
-          <h1>Mathieu B</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, dolor asperiores voluptate doloremque a dignissimos quos, aut expedita accusantium impedit ducimus tempora! Fuga modi architecto eligendi. Nostrum, debitis cum! A.</p>
-          <p>Architect & Engineer</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
 </div>
 
 
