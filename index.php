9e4c7030237a7fa06989dc63eb659eb3db089819
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-01-24 13:07:44 +0100

    Preparing the scrollmagic

--- index.php
+++ index.php
@@ -9,7 +9,13 @@
     <script src="libs/anime.min.js"></script>
     <script src="libs/pixi.min.js"></script>
     <script src="node_modules/animejs/lib/anime.js"></script>
-    <link rel="stylesheet" href="libs/animate.min.css"/>
+    <link rel="stylesheet" href="libs/animate.min.css" />
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
+
+    <script>
+        new WOW().init();
+    </script>
 </head>
 <link href="styles/main.css" rel="stylesheet" type="text/css" />
 
